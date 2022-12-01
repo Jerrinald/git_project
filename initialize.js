4d031c1a0125e0b90d61f73104b5eecc10340915
Jerrinald <jerrinald95190@live.fr>
2022-12-01 22:47:03 +0100

    Bumped version number to 1.2

--- initialize.js
+++ initialize.js
@@ -3,4 +3,4 @@ console.log("Hello world!");
 var date = new Date();
 var current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
 
-console.log('current_date');
+console.log(current_date);
